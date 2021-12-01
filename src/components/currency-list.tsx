02f54e789c3ce1d02316cfa522ede955e3e8f2d8
Radim Svoboda <radim.svoboda@emplifi.com>
2021-12-01 09:07:14 +0100

    display list of currencies - install styled-components

--- src/components/currency-list.tsx
+++ src/components/currency-list.tsx
@@ -1,14 +1,34 @@
+import {DataGrid} from '@mui/x-data-grid'
+import type {GridColDef} from '@mui/x-data-grid'
+import styled from 'styled-components'
 import {useFetchCnbCurrencyRates} from './../hooks/endpoints'
+
+const columns: GridColDef[] = [
+	{field: 'country', headerName: 'Country', width: 190},
+	{field: 'currency', headerName: 'Currency', width: 120},
+	{field: 'amount', headerName: 'Amount', align: 'right', width: 120},
+	{field: 'id', headerName: 'Symbol', width: 120},
+	{field: 'rate', headerName: 'Rate', align: 'right', width: 120},
+]
+
+const GridWrapper = styled.div`
+	height: 800px;
+	width: 800px;
+	margin: auto;
+`
 
 const CurrencyList = () => {
 	const {isLoading, error, data} = useFetchCnbCurrencyRates()
-
 	return (
 		<>
 			<h2>Currency List</h2>
 			{isLoading && 'Loading...'}
 			{error && `An error has occurred: ${error.message}`}
-			{data && JSON.stringify(data)}
+			{data && (
+				<GridWrapper>
+					<DataGrid disableColumnFilter rows={data} columns={columns} />
+				</GridWrapper>
+			)}
 		</>
 	)
 }
